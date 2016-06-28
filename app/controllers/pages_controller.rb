afbe2f78f81a9358cfc4b36077faae5ea8b29456
Manuel HA <nicamis@gmail.com>
2016-06-28 02:52:06 +0000

    Initial commit............................

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,15 @@
+class PagesController < ApplicationController
+  def index
+  end
+  def about_us
+  end
+
+  def contact_us
+  end
+
+  def tos
+  end
+
+  def faq
+  end
+end
---
+++ test/controllers/pages_controller_test.rb
@@ -0,0 +1,24 @@
+require 'test_helper'
+
+class PagesControllerTest < ActionController::TestCase
+  test "should get about_us" do
+    get :about_us
+    assert_response :success
+  end
+
+  test "should get contact_us" do
+    get :contact_us
+    assert_response :success
+  end
+
+  test "should get tos" do
+    get :tos
+    assert_response :success
+  end
+
+  test "should get faq" do
+    get :faq
+    assert_response :success
+  end
+
+end
